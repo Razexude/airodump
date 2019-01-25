1efdfd2e706bee4a0878985520acd2de29a528fd
umbum <umbum7601@gmail.com>
2019-01-26 02:09:26 +0900

    MacAddr: add operator overloading

--- src/MacAddr.h
+++ src/MacAddr.h
@@ -16,12 +16,21 @@ private:
     uint8_t addr[LENGTH];
 
 public:
+    MacAddr() {}
+    MacAddr(uint8_t *target) { memcpy(this->addr, target, LENGTH); }
+
     operator uint8_t*() { return addr; }
     void operator=(const uint8_t* target) { memcpy(this->addr, target, LENGTH); }
     bool operator==(const MacAddr& target) { return !memcmp(this->addr, target.addr, LENGTH); }
     bool operator==(const uint8_t* target) { return !memcmp(this->addr, target, LENGTH); }
     bool operator!=(const MacAddr& target) { return memcmp(this->addr, target.addr, LENGTH); }
     bool operator!=(const uint8_t* target) { return memcmp(this->addr, target, LENGTH); }
+
+    operator std::string() const { 
+        char buf[18];
+        sprintf(buf, "%02x:%02x:%02x:%02x:%02x:%02x", addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
+        return std::string(buf);
+    }
     friend std::ostream& operator<<(std::ostream& os, MacAddr& obj) { 
         return os << std::hex << std::setfill('0') <<
                         std::setw(2) << unsigned(obj.addr[0]) << ":" <<
@@ -33,8 +42,6 @@ public:
                     std::dec << std::setfill(' '); 
     }
 
-    char* toCString(char *str, size_t len) const;
-    void print() const;
 };
 
 #pragma pack(pop)
